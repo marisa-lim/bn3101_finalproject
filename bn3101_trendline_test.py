0dd49c24a5355b901adda95dd6a5978dd4a078a3
Marisa Lim <65386928+marisa-lim@users.noreply.github.com>
2021-10-27 15:22:48 +0800

    haven't done prediction yet

--- bn3101_trendline_test.py
+++ bn3101_trendline_test.py
@@ -1,109 +1,87 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import math
+
+# CREATE RANDOM DATASET
+
+# n_readings = total number of readings
+# n_ samples = total number of samples in each reading
+# frac_normal = fraction of total number of readings that will be normal aka no stenosis (a float between 0 to 1) 
+# min_val = minimum value of audio signal (usually zero)
+# max_val = maximum value of audio signal in normal reading (no stenosis)
+# increment = how much to increase each iteration's value by
+
+def create_data(n_readings, n_samples, frac_normal, min_val, max_val, increment): #n_samples in multiples of 5 please!
+  dataset = []
+
+  n_normal = (frac_normal)*n_readings #number of readings that are normal readings
+  for i in range(int(n_normal)):
+    dataset.append(np.array(np.random.uniform(min_val, max_val, n_samples)))
+  
+  n_rest = (1-frac_normal)*n_readings #the rest of the readings will show gradual increase in stenosis
+  new_max_val = max_val
+
+  for i in range(int(n_rest)):
+    #new_min_val += np.random.uniform(0,increment) #adding a random number to increase the min val - NOT IN USE ANYMORE
+    new_max_val += np.random.uniform(0,increment) #adding a random number to increase the max val
+    noise = list(np.random.uniform(min_val, max_val, int(0.2*n_samples)))
+    spike = list(np.random.uniform(min_val, new_max_val, int(0.6*n_samples)))
+    dataset.append(np.array(noise + spike + noise)) #assume all readings take the same structure
+  
+  return dataset
 
 ##########################################################################################################################################
-#################################################### STEP 1: CREATE DATA #################################################################
+
+# Plots graph for each reading within the created dataset
+
+def plot_dataset(dataset):
+  ymax = math.ceil(max(sample_data[len(dataset)-1]))
+  for i in range(len(dataset)):
+    plt.plot(sample_data[i])
+    plt.title('Reading %s' % (i+1))
+    plt.xlabel('Time')
+    plt.ylabel('Audio signal detected')
+    plt.grid(color="#D6EAF8")
+    plt.ylim(0,ymax)
+    plt.show()
+
 ##########################################################################################################################################
+
+# Plot one reading
+
+def plot_reading(reading, title, xlabel, ylabel):
+  ymax = math.ceil(max(reading))
+  plt.plot(reading)
+  plt.title(title)
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  plt.grid(color="#D6EAF8")
+  plt.ylim(0,ymax)
+  plt.show()
+
+##########################################################################################################################################
+
+def create_avglist(dataset):
+  avg_list = []
+  for i in range(len(dataset)):
+    avg_list.append(np.average(dataset[i]))
+  return avg_list
+
+#################################################### STEP 1: CREATE DATA #################################################################
 
 # Create base measurement data
 
-#noise = list(np.random.uniform(0, 2, 5)) # generate noise between 0-2
-#spikes_2 = list(np.random.uniform(4, 5, 5)) # generate noise between 4-5
-#spikes_3 = list(np.random.uniform(5, 6, 5)) # generate noise between 4-5
-#spikes_4 = list(np.random.uniform(8, 10, 5)) # generate noise between 4-5
-#spikes_5 = list(np.random.uniform(10, 12, 5)) # generate noise between 4-5
+sample_data = create_data(500, 300, 0.4, 0, 2, 0.05)
+#plot_reading(sample_data[0], 'Reading Plot', 'Time', 'Audio signal detected')
+#plot_reading(sample_data[499], 'Reading Plot', 'Time', 'Audio signal detected')
 
-# each reading will have 300 entries in one np array
-
-reading_1 = np.array(np.random.uniform(0, 2, 300)) # purely just noise - no stenosis
-reading_2 = np.array(np.random.uniform(0, 2, 300)) # purely just noise - no stenosis
-
-# some stenosis detected
-
-# noise spike noise spike
-reading_3 = np.array(list(np.random.uniform(0, 2, 150))+list(np.random.uniform(1, 3, 50))+list(np.random.uniform(0, 2, 50))+list(np.random.uniform(1, 3, 50)))
-# noise spike noise spike noise
-reading_4 = np.array(list(np.random.uniform(0, 2, 100))+list(np.random.uniform(1, 4, 50))+list(np.random.uniform(0, 2, 50))+list(np.random.uniform(1, 4, 50))+list(np.random.uniform(0, 2, 50)))
-# noise spike noise spike
-reading_5 = np.array(list(np.random.uniform(0, 2, 50))+list(np.random.uniform(1, 5, 100))+list(np.random.uniform(0, 2, 100))+list(np.random.uniform(1, 5, 50)))
-# noise spike noise spike noise
-reading_6 = np.array(list(np.random.uniform(0, 2, 100))+list(np.random.uniform(2, 5, 50))+list(np.random.uniform(0, 2, 50))+list(np.random.uniform(1, 5, 50))+list(np.random.uniform(0, 2, 50)))
-# noise spike noise spike
-reading_7 = np.array(list(np.random.uniform(0, 2, 150))+list(np.random.uniform(1, 6, 50))+list(np.random.uniform(0, 2, 50))+list(np.random.uniform(2, 5, 50)))
-# noise spike noise spike
-reading_8 = np.array(list(np.random.uniform(0, 2, 50))+list(np.random.uniform(2, 6, 100))+list(np.random.uniform(0, 2, 100))+list(np.random.uniform(2, 5, 50)))
-# noise spike noise spike noise
-reading_9 = np.array(list(np.random.uniform(0, 2, 100))+list(np.random.uniform(1, 5, 50))+list(np.random.uniform(0, 2, 50))+list(np.random.uniform(2, 7, 50))+list(np.random.uniform(0, 2, 50)))
-# noise spike noise spike
-reading_10 = np.array(list(np.random.uniform(0, 2, 50))+list(np.random.uniform(2, 7, 100))+list(np.random.uniform(0, 2, 100))+list(np.random.uniform(2, 8, 50)))
-
-##########################################################################################################################################
 ####################################################### STEP 2: CLEANING DATA ############################################################
-##########################################################################################################################################
 
-# Find the number of spikes and value of spikes for each reading
-reading_list = [reading_1, reading_2, reading_3, reading_4, reading_5, reading_6, reading_7, reading_8, reading_9, reading_10]
+# Find average audio signal per reading
 
-# get averages to compare
-avg_list = []
-for i in range(len(reading_list)):
-  avg_list.append(np.average(reading_list[i]))
+averages = create_avglist(sample_data)
+#plot_reading(averages, 'Average values across readings', 'Reading', 'Average audio signal')
 
-print("Average values for past reading are: ", avg_list)
+####################################################### STEP 3: PREDICTION ###############################################################
 
-# show average plot
-plt.plot(avg_list)
-plt.title('Trendline')
-plt.xlabel('Readings')
-plt.ylabel('Average signal')
-plt.ylim(0,5)
-plt.show()
-
-##########################################################################################################################################
-####################################################### OPTIONAL: PLOTTING DATA ##########################################################
-##########################################################################################################################################
-
-
-# plot each reading
-ymax = 10
-
-# reading 1 plot
-plt.plot(reading_1)
-plt.title('Reading 1')
-plt.xlabel('Time')
-plt.ylabel('Signal detected')
-plt.ylim(0,ymax)
-plt.show()
-
-# reading 2 plot
-plt.plot(reading_2)
-plt.title('Reading 2')
-plt.xlabel('Time')
-plt.ylabel('Signal detected')
-plt.ylim(0,ymax)
-plt.show()
-
-# reading 3 plot
-plt.plot(reading_3)
-plt.title('Reading 3')
-plt.xlabel('Time')
-plt.ylabel('Signal detected')
-plt.ylim(0,ymax)
-plt.show()
-
-# reading 4 plot
-plt.plot(reading_4)
-plt.title('Reading 4')
-plt.xlabel('Time')
-plt.ylabel('Signal detected')
-plt.ylim(0,ymax)
-plt.show()
-
-# reading 5 plot
-plt.plot(reading_5)
-plt.title('Reading 5')
-plt.xlabel('Time')
-plt.ylabel('Signal detected')
-plt.ylim(0,ymax)
-plt.show()
